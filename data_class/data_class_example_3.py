1b52d5f38e003418fc6024b5283685dd86554d39
nahid17511084 <203040na@#>
2022-03-30 19:37:20 +0600

    example 3 added

---
+++ data_class/data_class_example_3.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+
+@dataclass(order=True)
+class Person:
+    # we have to set field(init=False) to take it as not class param and not to init.
+    sort_index: int = field(init=False)
+    name: str
+    job: str
+    age: int
+
+    def __post_init__(self):
+        self.sort_index = self.age
+
+person1 = Person("Atawur vai", "Clan lead", 40)
+person2 = Person("Masud Vai", "Senior Soft Engr", 28)
+person3 = Person("Masud Vai", "Senior Soft Engr", 28)
+
+print(id(person1))
+print(id(person2))
+print(person1)
+print(person1.job)
+
+# here the comparison is returning false, where it should return true
+# It is not returning true cz we have not defined where to compare in data.
+# we can do it sort_index variable and post_init method.
+# post_init means, after initialization the values we get.
+print(person1 > person3)
+
+"""
+If we use data class as decorator we get a nice visual of the object
+not a conventional object. 
+
+We do not have to write init method to initialize parameters. 
+"""
+
