b6da8a7010ba0622e3f14a3253ffdf480bae8889
nahid17511084 <203040na@#>
2022-03-30 16:01:29 +0600

    data class example 1 added

---
+++ data_class/data_class_example_1.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+    name: str
+    job: str
+    age: int
+
+person1 = Person("Atawur vai", "Clan lead", 40)
+person2 = Person("Masud Vai", "Senior Soft Engr", 28)
+person3 = Person("Masud Vai", "Senior Soft Engr", 28)
+
+print(id(person1))
+print(id(person2))
+print(person1)
+print(person1.job)
+
+print(person2 == person3)
+
+"""
+If we use data class as decorator we get a nice visual of the object
+not a conventional object. 
+
+We do not have to write init method to initialize parameters. 
+"""
+
